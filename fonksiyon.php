528ee29624ceef1e4e686caa734de00af4afb1f5
Buyatek <cahit2834@gmail.com>
2026-02-17 11:45:02 +0300

    Update fonksiyon.php

--- fonksiyon.php
+++ fonksiyon.php
@@ -1,5 +1,23 @@
 <?php
 
+date_default_timezone_set('Europe/Istanbul');
+
+$bugunsaat=date("Y-m-d H:i:s");
+
+if ($_POST['bugun']==""){
+$bugun=date("Y-m-d");
+}else{
+$bugun=$_POST['bugun'];
+}
+
+if ($_POST['yarin']==""){
+$yarin = strtotime('1 day',strtotime($bugun));
+$yarin = date('Y-m-d' ,$yarin );
+}else{
+$yarin=$_POST['yarin'];
+}
+
+
 function dosyacevirkisa($gelen){
 		  $gelen=str_replace('/var/spool/asterisk/monitor/', '', $gelen);
 		  return $gelen;
